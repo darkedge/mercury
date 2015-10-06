f36a4d34780a582dd1821fa5904ac8f481eb6aa3
Marco Jonkers <contact@marcojonkers.com>
2015-10-06 21:57:21 +0200

    Add separate Win32 and x64 build configs

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
-if not exist build mkdir build
+if not exist mercury-x64 mkdir mercury-x64
 
-pushd build
+pushd mercury-x64
 set CommonCompilerFlags=-Od -I.. -I..\glfw-3.1.1\include -MTd -fp:fast -fp:except- -Gm- -GR- -EHa- -Oi -W4
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib glfw3dll.lib opengl32.lib -libpath:..\glfw-build\src\Release
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib glfw3dll.lib opengl32.lib -libpath:..\glfw-x64\src\Release
 
 cl %CommonCompilerFlags% ..\mercury.cpp /link %CommonLinkerFlags%
 popd
--- glfw-build.bat
+++ glfw-build.bat
@@ -1,5 +1,14 @@
-if not exist glfw-build mkdir glfw-build
-pushd glfw-build
-cmake -DBUILD_SHARED_LIBS=ON ..\glfw-3.1.1
-msbuild /v:m /p:Configuration="Release" src\glfw.vcxproj
+@echo off
+:: 32-bit shared build
+if not exist glfw-Win32 mkdir glfw-Win32
+pushd glfw-Win32
+cmake -G"Visual Studio 14 2015" -DBUILD_SHARED_LIBS=ON ..\glfw-3.1.1
+msbuild /v:m /p:Configuration="Release";OutDir="..\mercury-Win32\" src\glfw.vcxproj
+popd
+
+:: 64-bit shared build
+if not exist glfw-x64 mkdir glfw-x64
+pushd glfw-x64
+cmake -G"Visual Studio 14 2015 Win64" -DBUILD_SHARED_LIBS=ON ..\glfw-3.1.1
+msbuild /v:m /p:Configuration="Release";OutDir="..\mercury-x64\" src\glfw.vcxproj
 popd
