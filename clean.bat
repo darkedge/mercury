46e34fad72f0f4fb02fe723242a81784ea7cf062
Marco Jonkers <contact@marcojonkers.com>
2015-10-06 22:05:46 +0200

    Add clean batch file

---
+++ clean.bat
@@ -0,0 +1,5 @@
+@echo off
+if exist glfw-Win32 rmdir /s /q glfw-Win32
+if exist glfw-Win32 rmdir /s /q glfw-x64
+if exist glfw-Win32 rmdir /s /q mercury-Win32
+if exist glfw-Win32 rmdir /s /q mercury-x64
