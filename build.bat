73b35e4caee1b1ee031bf84398b4679dd8f29080
Marco Jonkers <contact@marcojonkers.com>
2015-10-06 22:38:51 +0200

    Add trivial warnings to ignore list

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if not exist mercury-x64 mkdir mercury-x64
 
 pushd mercury-x64
-set CommonCompilerFlags=-Od -I.. -I..\glfw-3.1.1\include -MTd -fp:fast -fp:except- -Gm- -GR- -EHa- -Oi -W4
+set CommonCompilerFlags=-nologo -Od -I.. -I..\glfw-3.1.1\include -MTd -fp:fast -fp:except- -Gm- -GR- -EHa- -Oi -W4 -WX -wd4201 -wd4100 -wd4189 -wd4505 -wd4127
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib glfw3dll.lib opengl32.lib -libpath:..\glfw-x64\src\Release
 
 cl %CommonCompilerFlags% ..\mercury.cpp /link %CommonLinkerFlags%
