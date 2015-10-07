4da2ce67f5ed6cfdc612c0e769dab69dbc7b90af
Marco Jonkers <contact@marcojonkers.com>
2015-10-07 16:52:46 +0200

    Cleaned up build commands

--- build.bat
+++ build.bat
@@ -2,9 +2,10 @@
 
 if not exist mercury-x64 mkdir mercury-x64
 
+:: x64-debug
 pushd mercury-x64
-set CommonCompilerFlags=-nologo -Od -I.. -I..\glfw-3.1.1\include -MTd -fp:fast -fp:except- -Gm- -GR- -EHa- -Oi -W4 -WX -wd4201 -wd4100 -wd4189 -wd4505 -wd4127
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib glfw3dll.lib opengl32.lib -libpath:..\glfw-x64\src\Release
+set CommonCompilerFlags=/nologo /Od /I".." /I"..\glfw-3.1.1\include" /MTd /fp:fast /fp:except- /Gm- /GR- /EHa- /Oi /W4 /WX /wd4201 /wd4100 /wd4189 /wd4505 /wd4127
+set CommonLinkerFlags=/INCREMENTAL:NO /OPT:REF "user32.lib" "gdi32.lib" "glfw3dll.lib" "opengl32.lib" /LIBPATH:"..\glfw-x64\src\Release"
 
 cl %CommonCompilerFlags% ..\mercury.cpp /link %CommonLinkerFlags%
 popd
