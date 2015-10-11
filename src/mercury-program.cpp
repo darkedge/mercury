f4a7270e6fe56cbda39925dc70cbf3518ce3a62c
Marco Jonkers <aba47c75@opayq.com>
2015-10-11 16:32:41 +0200

    Rework internal functions for programs

--- src/mercury-program.cpp
+++ src/mercury-program.cpp
@@ -1,19 +1,21 @@
 #include "mercury-program.h"
 
-static GLuint m_program = 0;
-static GLuint m_vertex = 0;
-static GLuint m_fragment = 0;
-static GLint u_mvp = -1;
-static GLint u_m = -1;
+static struct GeometryProgram {
+	GLuint program = 0;
+	GLuint vertex = 0;
+	GLuint fragment = 0;
+	GLint u_mvp = -1;
+	GLint u_m = -1;
+} s_geometryProgram;
 
-void AttachShaderFromFile(GLuint *shader, const char *file, GLenum shaderType) {
+GLuint _AttachShaderFromFile(GLuint program, const char *file, GLenum shaderType) {
 	size_t size = 0;
 #pragma warning(suppress:4996)
 	FILE *f = fopen(file, "rb");
 	if (!f)
 	{
 		printf("Could not open file: %s\n", file);
-		return;
+		return 0;
 	}
 
 	fseek(f, 0, SEEK_END);
@@ -24,54 +26,56 @@ void AttachShaderFromFile(GLuint *shader, const char *file, GLenum shaderType) {
 	{
 		delete [] str;
 		printf("Error while reading file: %s\n", file);
-		return;
+		return 0;
 	}
 	fclose(f);
 	str[size] = '\0';
 
-	*shader = glCreateShader(shaderType);
+	GLuint shader = glCreateShader(shaderType);
 
-	glShaderSource(*shader, 1, (const char **) &str, nullptr);
+	glShaderSource(shader, 1, (const char **) &str, nullptr);
 	delete [] str;
 
-	GL_TRY(glCompileShader(*shader));
+	GL_TRY(glCompileShader(shader));
 
 	// Check compilation log
 	GLint status;
-	GL_TRY(glGetShaderiv(*shader, GL_COMPILE_STATUS, &status));
-	bool success = (status == GL_TRUE);
-	if (success)
+	GL_TRY(glGetShaderiv(shader, GL_COMPILE_STATUS, &status));
+	if (status == GL_TRUE)
 	{
-		GL_TRY(glAttachShader(m_program, *shader));
+		GL_TRY(glAttachShader(program, shader));
 	}
 	else
 	{
 		GLint length = 0;
-		GL_TRY(glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &length));
+		GL_TRY(glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length));
 		if (length > 1)
 		{
 			char *c_log = new char[length];
-			GL_TRY(glGetShaderInfoLog(*shader, length, nullptr, c_log));
+			GL_TRY(glGetShaderInfoLog(shader, length, nullptr, c_log));
 			printf("%s\n", c_log);
 			delete [] c_log;
 		}
+		return 0;
 	}
+
+	return shader;
 }
 
-bool FindProgramErrors(GLenum type)
+bool _FindProgramErrors(GLuint program, GLenum type)
 {
 	GLint status;
-	GL_TRY(glGetProgramiv(m_program, type, &status));
+	GL_TRY(glGetProgramiv(program, type, &status));
 	bool success = (status == GL_TRUE);
 	if (!success)
 	{
 		GLint length = 0;
-		GL_TRY(glGetProgramiv(m_program, GL_INFO_LOG_LENGTH, &length));
+		GL_TRY(glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length));
 
 		if (length > 0)
 		{
 			char *c_log = new char[length];
-			GL_TRY(glGetProgramInfoLog(m_program, length, nullptr, c_log));
+			GL_TRY(glGetProgramInfoLog(program, length, nullptr, c_log));
 			printf("%s\n", c_log);
 			delete [] c_log;
 		}
@@ -83,27 +87,27 @@ bool FindProgramErrors(GLenum type)
 }
 
 void LoadProgram() {
-	m_program = glCreateProgram();
-	AttachShaderFromFile(&m_vertex, "../../assets/program.vert", GL_VERTEX_SHADER);
-	AttachShaderFromFile(&m_fragment, "../../assets/program.frag", GL_FRAGMENT_SHADER);
-	glLinkProgram(m_program);
-	FindProgramErrors(GL_LINK_STATUS);
-	glValidateProgram(m_program);
-	FindProgramErrors(GL_VALIDATE_STATUS);
+	s_geometryProgram.program = glCreateProgram();
+	s_geometryProgram.vertex = _AttachShaderFromFile(s_geometryProgram.program, "../../assets/program.vert", GL_VERTEX_SHADER);
+	s_geometryProgram.fragment = _AttachShaderFromFile(s_geometryProgram.program, "../../assets/program.frag", GL_FRAGMENT_SHADER);
+	glLinkProgram(s_geometryProgram.program);
+	_FindProgramErrors(s_geometryProgram.program, GL_LINK_STATUS);
+	glValidateProgram(s_geometryProgram.program);
+	_FindProgramErrors(s_geometryProgram.program, GL_VALIDATE_STATUS);
 
 	// get uniforms
-	u_mvp = glGetUniformLocation(m_program, "mvp_matrix");
-	u_m = glGetUniformLocation(m_program, "m_matrix");
+	s_geometryProgram.u_mvp = glGetUniformLocation(s_geometryProgram.program, "mvp_matrix");
+	s_geometryProgram.u_m = glGetUniformLocation(s_geometryProgram.program, "m_matrix");
 }
 
 void BindProgram() {
-	glUseProgram(m_program);
+	glUseProgram(s_geometryProgram.program);
 }
 
 void UploadMVPMatrix(const mat4 &mat) {
-	GL_TRY(glUniformMatrix4fv(u_mvp, 1, GL_FALSE, &mat.e[0]));
+	GL_TRY(glUniformMatrix4fv(s_geometryProgram.u_mvp, 1, GL_FALSE, &mat.e[0]));
 }
 
 void UploadMMatrix(const mat4 &mat) {
-	GL_TRY(glUniformMatrix4fv(u_m, 1, GL_FALSE, &mat.e[0]));
+	GL_TRY(glUniformMatrix4fv(s_geometryProgram.u_m, 1, GL_FALSE, &mat.e[0]));
 }
